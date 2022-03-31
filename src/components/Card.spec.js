d6913221eaf3ff13a104ab95a0b2012167b97cb4
Julia Schneider <julia_schneider_mail@posteo.de>
2022-03-31 16:16:50 +0200

    update card.spec

--- src/components/Card.spec.js
+++ src/components/Card.spec.js
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 import Card from './Card';
 
 describe('Card', () => {
-  it('renders a question, an answer and a button', () => {
-    render(<Card question="Whats up?" answer="Something." />);
+  it('renders a question, and two buttons', () => {
+    render(<Card question="Whats up?" />);
 
     const question = screen.getByText('Whats up?');
     const deleteButton = screen.getByRole('button', { name: /lösche/i });
