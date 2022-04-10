eaecadac5a702877d27877630ee9782b5334852c
Julia Schneider <julia_schneider_mail@posteo.de>
2022-04-10 16:27:33 +0200

    remove console.log

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,7 +21,6 @@ export default function Home({
   const [currentFilter, setCurrentFilter] = useState('');
   const [value, setValue] = useState('');
 
-  console.log(currentFilter);
   return (
     <GridWrapper>
       <FlexWrapper>
