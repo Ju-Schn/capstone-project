38daf8ce8fa40acc34d5f673f26b9be6e4920bc5
Julia Schneider <julia_schneider_mail@posteo.de>
2022-04-22 15:55:52 +0200

    update tests, install mongoose

--- src/pages/LandingPage.spec.js
+++ src/pages/LandingPage.spec.js
@@ -3,13 +3,15 @@ import { render, screen } from '@testing-library/react';
 import LandingPage from './LandingPage';
 
 describe('LandingPage', () => {
-  it('renders four navigation links', () => {
+  it('renders four navigation links, and a title', () => {
     render(
       <MemoryRouter>
         <LandingPage />
       </MemoryRouter>
     );
 
+    const Title = screen.getByText(/willkommen/i);
+    expect(Title).toBeInTheDocument();
     const navigationLinks = screen.getAllByRole('link');
     expect(navigationLinks).toHaveLength(4);
   });
